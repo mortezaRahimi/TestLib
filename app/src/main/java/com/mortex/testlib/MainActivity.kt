24f87b24ea9e5fab7876b7243fd4298d04bb3f1d
morteza <rahimi.morteza53266@gmail.com>
2022-12-20 10:52:42 +0330

    Init

---
+++ app/src/main/java/com/mortex/testlib/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mortex.testlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mortex.imagelib.ImageLib
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ imageLib/src/main/java/com/mortex/imagelib/ImageLib.kt
@@ -0,0 +1,11 @@
+package com.mortex.imagelib
+
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.drawable.shapes.RoundRectShape
+
+class ImageLib(context:Context, outerRadii: FloatArray?, inset: RectF?, innerRadii: FloatArray?) :
+    RoundRectShape(outerRadii, inset, innerRadii) {
+
+
+}
